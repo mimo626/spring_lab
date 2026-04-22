528fcc9475eaa3044e8bf32c61e68f9b413ad724
kangminju <yessuk5499@gmail.com>
2026-04-22 20:29:53 +0900

    LottoVO2.java 구현

---
+++ src/main/java/com/example/springlab/domain/LottoVO2.java
@@ -0,0 +1,11 @@
+package com.example.springlab.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LottoVO2 {
+    private String result;
+    private String imgName;
+}
