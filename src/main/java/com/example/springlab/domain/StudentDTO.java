8d375f20c477699798fb270c5dc1d2041c6e0629
kangminju <yessuk5499@gmail.com>
2026-04-27 18:55:10 +0900

    StudentDTO 구현

---
+++ src/main/java/com/example/springlab/domain/StudentDTO.java
@@ -0,0 +1,11 @@
+package com.example.springlab.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StudentDTO {
+    private String name;
+    private int score;
+}
