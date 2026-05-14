43094f667b99eade98fc7665a814170190e3fe54
kangminju <yessuk5499@gmail.com>
2026-05-14 17:52:31 +0900

    app 설정

--- src/main/java/com/example/springlab/SpringlabApplication.java
+++ src/main/java/com/example/springlab/SpringlabApplication.java
@@ -3,10 +3,11 @@ package com.example.springlab;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication //(exclude = {DataSourceAutoConfiguration.class})
 @EnableScheduling
 @ComponentScan(basePackages = {"com.example.springlab", "thymeleaf.exam"})
 @MapperScan(value={"mybatis.dao"})
