04dc0b887eb92bd1288b2c0e513c92f8d2319a26
kangminju <yessuk5499@gmail.com>
2026-05-14 17:51:51 +0900

    Friend 생성

---
+++ src/main/java/com/example/springlab/domain/entity/Friend.java
@@ -0,0 +1,24 @@
+package com.example.springlab.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.*;
+import jakarta.persistence.Id;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Data
+@ToString
+@Entity
+public class Friend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column
+    private String fname;
+    @Column
+    private Integer fage;
+}
