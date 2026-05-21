5236b571842af91e1916acc874c453814e528046
kangminju <yessuk5499@gmail.com>
2026-05-21 17:35:17 +0900

    FriendRepository 쿼리문 추가

--- src/main/java/com/example/springlab/repository/FriendRepository.java
+++ src/main/java/com/example/springlab/repository/FriendRepository.java
@@ -4,8 +4,11 @@ import com.example.springlab.domain.entity.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FriendRepository extends JpaRepository<Friend, Integer> {
     Friend findByFname(String Fname);
+    List<Friend> findAllByFname(String Fname);
 
 }
